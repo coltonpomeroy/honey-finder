bad1ce1b96f426e3f4e6d4991352557da108c2b8
Colton Pomeroy <53071181+coltonpomeroy@users.noreply.github.com>
2024-12-28 16:12:14 -0600

    chore: refactor locations table

--- app/settings/page.js
+++ app/settings/page.js
@@ -5,7 +5,7 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Modal from '@/components/Modal';
 import Link from 'next/link';
-import { set } from 'mongoose';
+import LocationsTable from './LocationTable';
 
 export default function Settings() {
   const { data: session } = useSession();
@@ -386,51 +386,13 @@ export default function Settings() {
             </button>
           </div>
             {/* Locations Table */}
-            <div className="overflow-x-auto">
-              <table className="min-w-full bg-white border border-gray-200">
-                <thead>
-                  <tr>
-                    <th className="px-4 py-2 border-b">Location Name</th>
-                    <th className="px-4 py-2 border-b">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {locations.map((location) => (
-                    <tr
-                      key={location.id}
-                      className="hover:bg-gray-100 cursor-pointer"
-                      onClick={() => handleLocationChange(location)}
-                    >
-                      <td className="px-4 py-2 border-b">{location.name}</td>
-                      <td className="px-4 py-2 border-b">
-                        {selectedLocation?.id === location.id && (
-                          <div className="flex space-x-2">
-                            <button
-                              onClick={(e) => {
-                                e.stopPropagation();
-                                handleEditLocation(location);
-                              }}
-                              className="px-2 py-1 bg-green-500 text-white rounded hover:bg-green-600"
-                            >
-                              Edit
-                            </button>
-                            <button
-                              onClick={(e) => {
-                                e.stopPropagation();
-                                handleDeleteLocation(location);
-                              }}
-                              className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
-                            >
-                              Delete
-                            </button>
-                          </div>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+            <LocationsTable
+              locations={locations}
+              selectedLocation={selectedLocation}
+              handleLocationChange={handleLocationChange}
+              handleEditLocation={handleEditLocation}
+              handleDeleteLocation={handleDeleteLocation}
+            />
 
             {/* Containers Table */}
             {selectedLocation && containers.length > 0 && (
@@ -541,7 +503,7 @@ export default function Settings() {
                 </div>
               </div>
             )}
-            {currentLocation && !session?.user?.name && (
+            {currentLocation && (
               <div className="flex justify-end mt-4">
                 <button
                   onClick={confirmEditLocation}
