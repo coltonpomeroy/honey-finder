5f4f383eb2528021089c46d50ed57a5d054e6d70
Colton Pomeroy <53071181+coltonpomeroy@users.noreply.github.com>
2025-01-08 16:10:26 -0600

    fix: mobile items route

--- app/api/user/items/mobile/route.js
+++ app/api/user/items/mobile/route.js
@@ -1,50 +1,20 @@
 import { NextResponse } from 'next/server';
 import connectMongo from '@/libs/mongoose';
 import User from '@/models/User';
-import { OAuth2Client } from 'google-auth-library';
 
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
-async function verifyIdToken(token) {
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.GOOGLE_ID_OS,
-  });
-  const payload = ticket.getPayload();
-  return payload; // Contains user info like email, sub (user ID), etc.
-}
-
-export async function GET(req) {
+export async function POST(req) {
   await connectMongo();
 
   console.log({ headers: req.headers });
 
-  // Extract the token from the Authorization header
-  const authHeader = req.headers.get('authorization');
-  console.log({ authHeader });
-  
-  let token;
-  if (authHeader && authHeader.startsWith('Bearer ')) {
-    token = authHeader.substring(7, authHeader.length);
+  // Extract email from the request body
+  const { email } = await req.json();
+  if (!email) {
+    return NextResponse.json({ message: 'Email is required' }, { status: 400 });
   }
 
-  console.log({ tokenFromHeader: token });
-
-  // Validate the token using Google OAuth2 client
-  let decodedToken;
-  try {
-    decodedToken = await verifyIdToken(token);
-    console.log("Decoded token:", decodedToken);
-  } catch (error) {
-    console.error("Error verifying token:", error);
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-  }
-
-  if (!decodedToken) {
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-  }
-
-  const user = await User.findOne({ email: decodedToken.email });
+  // Check if the user exists in the database
+  const user = await User.findOne({ email });
 
   console.log({ user });
 
@@ -52,6 +22,7 @@ export async function GET(req) {
     return NextResponse.json({ message: 'User not found' }, { status: 404 });
   }
 
+  // Fetch the items for the authenticated user
   try {
     const items = await getAllItemsForUser(user._id);
     if (!items) {
@@ -60,7 +31,7 @@ export async function GET(req) {
 
     return NextResponse.json({ items }, { status: 200 });
   } catch (error) {
-    console.error("Error fetching items:", error);
+    console.error('Error fetching items:', error);
     return NextResponse.json({ message: 'Error fetching items' }, { status: 500 });
   }
 }
@@ -81,18 +52,18 @@ async function getAllItemsForUser(userId) {
             $cond: {
               if: { $eq: ['$storage.items.expirationDate', null] },
               then: new Date('9999-12-31'),
-              else: '$storage.items.expirationDate'
-            }
+              else: '$storage.items.expirationDate',
+            },
           },
           locationId: '$storage._id',
-          locationName: '$storage.name'
-        }
+          locationName: '$storage.name',
+        },
       },
       {
         $sort: {
-          expiration: 1
-        }
-      }
+          expiration: 1,
+        },
+      },
     ]);
 
     return userItems;
